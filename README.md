ce2e7613d8a7a89d55415ec1a4b3b0a9fe5417a4
Abe G <abram.2197@gmail.com>
2019-04-15 10:33:13 +0200

    init repo, moving from local to remote

---
+++ README.md
@@ -0,0 +1,16 @@
+#42-Taskmaster
+Just a little python module that fetchs the ETH/USD ticker in real time from the Delta's Exchange API.
+
+You can either launch the main script wich will call the module with
+
+```
+$/> python3 main.py
+```
+
+or you can also launch the module directly and it will fetch the ticker for you too with
+
+```
+$/> python3 tickerfetcher.py
+```
+
+dependencies: yaml
---
+++ main.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+"""Main module for this taskmaster project"""
+import sys
+import termios
+import tty
+import yaml
+
+def main():
+	"""main function"""
+	if len(sys.argv) != 2:
+		print("usage: main.py config_file")
+		exit(1)
+	with open(sys.argv[1], 'r') as stream:
+		try:
+			print(yaml.safe_load(stream))
+		except yaml.YAMLError as exc:
+			print(exc)
+	stdin = sys.stdin
+	fd = stdin.fileno()
+
+	new = old = termios.tcgetattr(fd)
+	new[3] &= ~termios.ICANON
+
+	termios.tcsetattr(fd, termios.TCSAFLUSH, new)
+	tty.setraw(sys.stdin)
+	char = stdin.read(1)
+
+	termios.tcsetattr(fd, termios.TCSAFLUSH, old)
+
+if __name__ == '__main__':
+	main()
