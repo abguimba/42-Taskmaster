acc0d412fdad0ee75177e5486cc87a6a2b1f04e6
Abe G <abram.2197@gmail.com>
2019-05-01 16:27:43 +0200

    clean user input

--- Pysrcs/userinput.py
+++ Pysrcs/userinput.py
@@ -9,12 +9,12 @@ import curses
 def ask_for_confirmation(programList, time):
 	"""Asks for confirmation of current setup"""
 	output.display_summary(programList, time)
-	confirmation = input()
+	confirmation = input().lower()
 	while confirmation != 'y' and confirmation != "yes":
 		if confirmation == 'n' or confirmation == "no":
 			print(output.bcolors.FAIL, "\n/!\\Aborting execution /!\\",
 			output.bcolors.ENDC)
 			sys.exit(0)
 		print("Please answer with yes/y or no/n")
-		confirmation = input()
+		confirmation = input().lower()
 	print('\n', end='')
